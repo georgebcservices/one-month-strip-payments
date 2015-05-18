60bed7939140295ae51007640b1cb1675d529924
georgebcservices <georgebcservices@gmail.com>
2015-05-18 13:04:20 -0500

    Added purchase model and created the db tables

--- db/migrate/20150518173258_create_purchases.rb
+++ db/migrate/20150518173258_create_purchases.rb
@@ -2,12 +2,12 @@ class CreatePurchases < ActiveRecord::Migration
   def change
     create_table :purchases do |t|
     	t.string :email
-    	t.interger :amount
+    	t.integer :amount
     	t.string :description
     	t.string :currency
     	t.string :customer_id
     	t.string :card
-    	t.interger :product_id
+    	t.integer :product_id
       t.timestamps null: false
     end
   end
