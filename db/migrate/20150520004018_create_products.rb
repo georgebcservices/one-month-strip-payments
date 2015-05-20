8b508738357f05fd1d5b1a97917b18e6932ce22d
georgebcservices <georgebcservices@gmail.com>
2015-05-19 19:43:35 -0500

    fix seed import

---
+++ db/migrate/20150520004018_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration
+ def change
+   create_table :products do |t|
+     t.string :title
+     t.string :subtitle
+     t.string :author
+     t.string :length
+     t.string :author_image_name
+     t.decimal :price
+     t.string :sku
+     t.string :download_url
+     t.text :description
+     t.text :author_description
+     t.string :details:
+     t.timestamps
+   end
+ end
