7192d31db9b34ebc9f0a09fb7b2449b633ff7ea3
georgebcservices <georgebcservices@gmail.com>
2015-05-21 14:33:15 -0500

    Added aproducts route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,18 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root 'pages#home'
   
+  
+
   # Support route for charges
-  resources :charges
+  resources :charges, only: [:show]
+
+  # Route for showing products
+  resources :products, only: [:show]
 
   # Route for showing Purchases
   resources :purchases, only: [:show]
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
