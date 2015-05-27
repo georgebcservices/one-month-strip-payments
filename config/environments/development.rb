d3cb8bd80ef12acd2af28520d2cac02a47e8d9c5
georgebcservices <georgebcservices@gmail.com>
2015-05-26 19:43:34 -0500

    saving cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Configure static file server for tests with Cache-Control for performance.
+  config.serve_static_files = false
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
